1dbea72843c8e1f515f5652c67881c74604e6761
Andrew <andrewen2010@yahoo.com>
2018-04-28 14:51:26 +0200

    fix bugs

--- src/com/rules/Operator.java
+++ src/com/rules/Operator.java
@@ -9,43 +9,27 @@ public class Operator {
 	
 	public boolean parse(PriorityQueue<Lexeme> lexemes) {
 		Lexeme l = lexemes.peek();
-		
+
 		if(l!=null&&(l.relatedToken.name.equals("PLUS")
 				||l.relatedToken.name.equals("MINUS")
 				||l.relatedToken.name.equals("MULTIPLY")
 				||l.relatedToken.name.equals("DIV")
 				||l.relatedToken.name.equals("AND")
 				||l.relatedToken.name.equals("OR"))
+				||l.relatedToken.name.equals("GREATER_EQ")
+				||l.relatedToken.name.equals("LESS_EQ")
+				||l.relatedToken.name.equals("NOT_EQUAL")
+				||l.relatedToken.name.equals("EQUAL")				
 				) {
 			lexemes.poll();
 			operator=l.matchedWord;
-			return true;
-		}
-		
-		else if(l!=null&&(l.relatedToken.name.equals("GREATERTHAN")||l.relatedToken.name.equals("LESSTHAN"))) {
-			lexemes.poll();
-			operator=l.matchedWord;
-			l = lexemes.peek();
-			if(l!=null&&l.relatedToken.name.equals("EQUAL")) {
-				operator+=l.matchedWord;
-				lexemes.poll();
-			}
-			return true;
-		}
-		else if(l!=null&&l.relatedToken.name.equals("NOT")) {
-			Lexeme l2 = lexemes.peek();
-			if(l2!=null&&l2.relatedToken.name.equals("EQUAL"))
-				lexemes.poll();
-				lexemes.poll();
-				operator=l.matchedWord;
-				operator+=l2.matchedWord;
 			return true;
 		}
 		return false;
 	}
 	
 	public void print() {
-		System.out.print(operator);
+		System.out.print(" "+operator);
 	}
 
 }
